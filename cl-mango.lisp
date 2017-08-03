4ec1c0dffb8579ea549b6e7f9f97d5cd47fce63f
Clint Moore <clint@ivy.io>
2017-08-03 05:05:52 -0700

    find-explicit

--- cl-mango.lisp
+++ cl-mango.lisp
@@ -38,13 +38,15 @@
   (defun symb (a b)
     (intern (format nil "~a-~a" (symbol-name a) (symbol-name b)))))
 
+(defun make-request-uri (req-path)
+    (with-output-to-string (sink)
+      (puri:render-uri (make-instance 'puri:uri :scheme *mango-scheme*
+                                                :host *mango-host*
+                                                :port *mango-port*
+                                                :path req-path) sink)))
+
 (defmacro make-couchdb-request (path &key (parameters nil) (content nil) (method :get))
-  `(labels ((make-request-uri (req-path)
-              (with-output-to-string (sink)
-                (puri:render-uri (make-instance 'puri:uri :scheme *mango-scheme*
-                                                          :host *mango-host*
-                                                          :port *mango-port*
-                                                          :path req-path) sink))))
+  `(labels ()
      
      (let ((response (drakma:http-request
                       (make-request-uri ,path)
@@ -58,6 +60,7 @@
                       :preserve-uri t
                       :external-format-in :utf8
                       :external-format-out :utf8
+                      :connection-timeout 60
                       ,@(when parameters `(:parameters ,parameters))
                       ,@(when content `(:content ,content)))))
        (cond ((equal (type-of response) '(simple-vector 2)) (flexi-streams:octets-to-string response))
@@ -86,13 +89,21 @@
   (make-couchdb-request (format nil "/~a/~a?rev=~a" db docid revision)
                         :method :delete))
 
-(defun make-selector (parameters)
-  (check-type parameters list)
-  (with-output-to-string (sink)
-    (yason:encode (alist-hash-table
-                   (list (cons "limit" 10000)
-                         (cons "selector"
-                                  (alist-hash-table parameters)))) sink)))
+(defmacro make-selector (selector &key (limit 100) fields sort skip)
+  (let ((the-request (gensym)))
+    `(let ((,the-request (with-output-to-string (sink)
+                           (yason:encode (alist-hash-table (list (cons "limit" ,limit)
+                                                                 ,@(when skip
+                                                                     `((cons "skip" ,skip)))
+                                                                 ,@(when sort
+                                                                     `((cons "sort" ,sort)))
+                                                                 ,@(when fields
+                                                                     `((cons "fields" ,fields)))
+                                                                 (cons "selector" (alist-hash-table ,selector))))
+                                         sink))))
+       (log:info ,the-request)
+       ,the-request)))
+
 
 (defun class-ify-couch-response (bundle class &key (doc-name "docs"))
   (check-type bundle string)
@@ -119,9 +130,8 @@
 
 (defmacro defmango (name slot-definitions)
   (let* ((exports (mapcan (lambda (spec)
-                            (when (getf (cdr spec) :export)
-                              (let ((name (getf (cdr spec) :accessor)))
-                                (list name))))
+                            (let ((name (getf (cdr spec) :accessor)))
+                              (list name)))
                           slot-definitions))
          (name-string (format nil "~a" name))
          (name-symbol (intern (symbol-name name)))
@@ -138,7 +148,9 @@
                            ,@slot-definitions)
          (:metaclass json-serializable-class))
 
-       ;; Export symbols for all accessors marked as 'export'
+       (export ',(symb name 'id))
+       (export ',(symb name 'rev))
+       
        ,@(mapcar (lambda (name) `(export ',name))
                  exports)
 
@@ -146,21 +158,36 @@
          (mapcar #'(lambda (doc)
                      (json-mop:json-to-clos (gethash "doc" doc) ',name-symbol))
                  (gethash "rows" (yason:parse (doc-get-all ,name-db-name)))))
-
+       (export ',(symb name 'get-all))
+       
        (defun ,(symb name 'put) (object)
          (mango-update ,name-db-name object))
+       (export ',(symb name 'put))
        
        (defun ,(symb name 'update) (object)
          (mango-update ,name-db-name object))
-
+       (export ',(symb name 'update))
+       
+       (defmacro ,(symb name 'find-explicit) (query &rest args)
+         `(class-ify-couch-response
+           (doc-find ,(string-downcase ,name-string) (make-selector ,query ,@args))
+           ',',name-symbol))
+       (export ',(symb name 'find-explicit))
+       
        (defun ,(symb name 'find) (query)
          (mango-find ,name-db-name ',name-symbol query))
-
+       (export ',(symb name 'find))
+       
        (defun ,(symb name 'delete) (object)
          (doc-delete ,name-db-name (,(symb name :id) object) (,(symb name :rev) object)))
-       
+       (export ',(symb name 'delete))
+
        (defmacro ,(symb name 'create) (&rest args)
          (let ((new-name (gensym)))
            `(let ((,new-name (make-instance ',',name-symbol ,@args)))
-              (,',(symb name :put) ,new-name)))))))
-
+              (,',(symb name :put) ,new-name))))
+       (export ',(symb name 'create))
+       
+       ,@ (mapcar (lambda (name)
+                    `(export ',name))
+                  exports))))
